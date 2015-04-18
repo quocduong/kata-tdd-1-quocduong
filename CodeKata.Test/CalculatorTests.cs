477418af4eab8fb13c2598d516cf2e385f836787
quocduong <duongdangquoc@gmail.com>
2015-04-18 23:51:13 +0700

    Implemented a simplest TDD demo

---
+++ CodeKata.Test/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeKata.Test
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddEmptyString()
+        {
+            var result = Calculator.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void AddOneNumberString(string numbers, int expected)
+        {
+            int result = Calculator.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("2,2", 4)]
+        public void AddTwoNumberString_ReturnSum(string numbers, int expected)
+        {
+            int result = Calculator.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2,3", 6)]
+        public void AddMultiple_ReturnSum(string numbers, int expected)
+        {
+            int result = Calculator.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_NoNumberStringWithDelimeter_ReturnZero()
+        {
+            int result = Calculator.Add("//,\n");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_OneNumberStringWithDelimeterLine_ReturnNumber()
+        {
+            int result = Calculator.Add("//,\n1");
+
+            Assert.AreEqual(1,result);
+        }
+
+        [Test]
+        public void AddTwoNumberWithAlternativeDelimeter_ReturnSum()
+        {
+            int result = Calculator.Add("//,\n1,2");
+            Assert.AreEqual(3, result);
+            Console.Write(result);
+        }
+    }
+}
---
+++ CodeKata/Calculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class Calculator
+    {
+        private const string _delimeterLine_indicator = "//";
+        private static string _delimeter = ",";
+
+        public static int Add(string numbers)
+        {
+            if (HasdelimeterLine(numbers))
+            {
+                ParseDelimeter(numbers);
+                numbers = GetNumbers(numbers);
+            }
+            if (IsEmptyString(numbers))
+                return HandleEmptyString();
+
+            if (HasMultipleNumbers(numbers))
+            {
+                return HandleMultipleNumber(numbers);
+            }
+
+            return HandleOneNumber(numbers);
+        }
+
+        private static void ParseDelimeter(string numbers)
+        {
+            _delimeter = numbers.Substring(2, 1);
+        }
+
+        private static string GetNumbers(string numbers)
+        {
+            string[] numParts = numbers.Split(char.Parse("\n"));
+            numbers = numParts[1];
+            return numbers;
+        }
+
+        private static bool HasdelimeterLine(string numbers)
+        {
+            return numbers.StartsWith(_delimeterLine_indicator);
+        }
+
+        private static int HandleMultipleNumber(string numbers)
+        {
+            string[] nums = numbers.Split(_delimeter.ToCharArray());
+
+            var total = 0;
+            foreach (var num in nums)
+            {
+                total += HandleOneNumber(num);
+            }
+            return total;
+            return HandleOneNumber(nums[0]) + HandleOneNumber(nums[1]);
+        }
+
+        private static bool HasMultipleNumbers(string numbers)
+        {
+            return numbers.Contains(_delimeter);
+        }
+
+        private static int HandleOneNumber(string numbers)
+        {
+            return int.Parse(numbers);
+        }
+
+        public static int HandleEmptyString()
+        {
+            return 0;
+        }
+
+        public static bool IsEmptyString(string numbers)
+        {
+            return string.IsNullOrEmpty(numbers);
+        }
+    }
+}
